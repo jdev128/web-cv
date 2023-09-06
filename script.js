19fd4f313e583a98221929394ad710a6a7ed4c5e
Mauricio Pisco <mauriciopisco@gmail.com>
2023-09-05 21:10:10 -0300

    Actualizar capacitaciones y competencias

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var age = 0;
 const MILLISECONDS_IN_A_YEAR = 60 * 1000 * 60 * 24 * 365;
 
 function calculateAge() {
-    age = Math.round((Date.now() - new Date("November 4, 1994").getTime()) / (MILLISECONDS_IN_A_YEAR));
+    let age = Math.floor((Date.now() - new Date("November 4, 1994").getTime()) / (MILLISECONDS_IN_A_YEAR));
     document.getElementById("age").innerHTML += `${age} Años`;
 }
 
+function calculateSeniority() {
+    let seniority = Math.floor((Date.now() - new Date("August 5, 2019").getTime()) / (MILLISECONDS_IN_A_YEAR));
+    document.getElementById("seniority").innerHTML = seniority;
+}
+
 window.addEventListener("load", calculateAge);
+window.addEventListener("load", calculateSeniority);
