c752b4b4913dd18ad8839c8e66e57c4b038c59c5
Mauricio Pisco <53369240+jdev128@users.noreply.github.com>
2025-04-25 12:16:53 +0000

    Translate to english

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const MILLISECONDS_IN_A_YEAR = 60 * 1000 * 60 * 24 * 365;
 
 function calculateAge() {
     let age = Math.floor((Date.now() - new Date("November 4, 1994").getTime()) / (MILLISECONDS_IN_A_YEAR));
-    document.getElementById("age").innerHTML += `${age} Años`;
+    document.getElementById("age").innerHTML += `${age} years`;
 }
 
 function calculateSeniority() {
